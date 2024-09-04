e5222c30d057aac8a1dbcd3079c2e0f19fcbd2b6
rafaelsavyo <rafaelblg.rb@gmail.com>
2024-09-04 16:02:13 -0300

    primeiro commit

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+c = 1
+while (c <= 6) {
+    console.log('Tudo bem ?')
+    c += 1
+}
+
+
+c = 1
+do {
+    console.log('Tudo bem ?')
+    c += 1
+} while (c <= 6)
---
+++ aula14/ambiente.js
@@ -0,0 +1,3 @@
+for (var c = 1; c <= 5; c += 2) {
+    console.log(c)
+}
